0c3e70090059979198aadb222539da7965a80765
Alexander M Artamonov <art.alex.m@gmail.com>
2021-12-20 09:18:18 +0300

    dw.laravel-11 Комментарии к новости

---
+++ tests/Feature/Http/Controllers/Site/ArticleControllerCommentsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Site;
+
+use App\Models\Article\Article;
+use App\Repositories\Site\ArticleCommentRepository;
+use Tests\TestCase;
+
+/**
+ * Class ArticleControllerCommentsTest.
+ *
+ * @package Tests\Feature\Http\Controllers\Site
+ */
+class ArticleControllerCommentsTest extends TestCase
+{
+    /**
+     * Проверяет, что детальный просмотр отдает нужные комментарии.
+     *
+     * @return void
+     */
+    public function test_commentsIsShouwn()
+    {
+        $item = Article::query()->published()->firstOrFail();
+
+        $url = route('news.detail', $item, false);
+        $response = $this->get($url);
+
+        $response->assertOk();
+
+        /** @var \Illuminate\Database\Eloquent\Collection $comments */
+        $comments = app(ArticleCommentRepository::class)->getPublished(20, $item->id);
+        $data = $comments->flatMap(fn($item) => [
+            $item->user->name,
+            $item->created_at,
+            $item->text,
+        ])->toArray();
+
+        $data = array_merge([
+            '<h4 class="mt-5">Комментарии</h4>',
+            '<div id="comment-list">'
+        ], $data);
+
+        $response->assertSeeInOrder($data, false);
+    }
+}
